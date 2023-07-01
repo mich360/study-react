4d21e58d2cfb399d4709e33d8800aecdb72c97a6
Mich <65708357+mich360@users.noreply.github.com>
2023-07-01 19:02:57 +0900

    Update MyComponent.tsx

--- pages/MyComponent.tsx
+++ pages/MyComponent.tsx
@@ -3,18 +3,17 @@ import styles from './MyComponent.module.css';
 import Lottie from 'lottie-react';
 import animationData from './animation.json';
 
-const Animation = () => {
+const MyComponent = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.myComponentTitle}>Comics!</h1>
       <p className={styles.customText}>This is my custom component.</p>
       <p style={{ color: 'red', fontWeight: 'bold' }}>Lottie animation</p>
       <div className={styles.lottieAnimation}>
-        <Lottie animationData={animationData} />
+        <Lottie animationData={animationData} loop autoplay />
       </div>
     </div>
   );
 };
 
-export default Animation;
- 
+export default MyComponent;
